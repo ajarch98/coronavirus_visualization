fef7ee972ee7104c75004ebbbaf4d77044522fe6
Advait Joshi <advaitjoshi1998@gmail.com>
2020-03-21 15:05:22 +0300

    Update to stable dataset source

--- main.py
+++ main.py
@@ -5,43 +5,43 @@ import pandas as pd
 
 class Grapher():
     def __init__(self):
-        self.url = 'https://docs.google.com/spreadsheets/d/18X1VM1671d99V_yd-cnUI1j8oSG2ZgfU_q1HfOizErA/export?format=csv&id'
-        self.data = pd.read_csv(self.url)
-        self.data = self.data.fillna(0)
+        self.confirmed_cases_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
+        self.deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+        self.confirmed_data = pd.read_csv(self.confirmed_cases_url)
+        self.deaths_data = pd.read_csv(self.deaths_url)
+        assert len(self.confirmed_data.columns) == len(self.deaths_data.columns)
 
     def create_graph(self):
         fig = go.Figure()
-        col_names = self.data.columns
+        col_names = self.confirmed_data.columns
 
         date_list = []
-        for i in range(5, len(self.data.columns), 2):
-            cases_col = i
-            cases_col_name = col_names[cases_col]
-            deaths_col = i + 1
-            deaths_col_name = col_names[deaths_col]
+        for i in range(4, len(self.confirmed_data.columns)):
 
-            df = self.data[['latitude', 'longitude', 'country', 'location', col_names[cases_col], col_names[deaths_col]]]
-            date = df[col_names[deaths_col]].name[7:17]
+            confirmed_df = self.confirmed_data
+            deaths_df = self.deaths_data
+
+            date = confirmed_df[col_names[i]].name
             date_list.append(date)
 
-            df_cases = df[df[cases_col_name] != 0]
-            df_cases['text'] = df_cases['country'] + '<br>' + df_cases['location'] + '<br>' + 'confirmed cases: ' + (df_cases[df_cases.columns[-2]].astype(int)).astype(str) + '<br>' +\
-                'deaths: ' + (df_cases[df_cases.columns[-1]].astype(int))\
-                .astype(str)
-            cases_lat = df_cases['latitude']
-            cases_lng = df_cases['longitude']
+            confirmed_df = confirmed_df[confirmed_df[date] != 0]
+            confirmed_lat = confirmed_df['Lat']
+            confirmed_lng = confirmed_df['Long']
+            confirmed_df['text'] = 'Country/Region: ' + confirmed_df['Country/Region'] + '<br>' + 'Confirmed cases: ' +\
+                confirmed_df[date].astype(str) + '<br>' +\
+                'deaths: ' + deaths_df[date].astype(str)
 
-            df_deaths = df[df[deaths_col_name] !=0 ]
-            deaths_lat = df_deaths['latitude']
-            deaths_lng = df_deaths['longitude']
-            df_deaths['text'] = df_deaths['country'] + '<br>' + df_deaths['location'] + '<br>' + 'confirmed cases: ' + (df_deaths[df_deaths.columns[-2]].astype(int)).astype(str) + '<br>' +\
-                'deaths: ' + (df_deaths[df_deaths.columns[-1]].astype(int))\
-                .astype(str)
+            deaths_df = deaths_df[deaths_df[date] != 0]
+            deaths_lat = deaths_df['Lat']
+            deaths_lng = deaths_df['Long']
+            deaths_df['text'] = 'Country/Region: ' + deaths_df['Country/Region'] + '<br>' + 'Confirmed cases: ' +\
+                confirmed_df[date].astype(str) + '<br>' +\
+                'Deaths: ' + deaths_df[date].astype(str)
 
-            fig.add_trace(go.Scattergeo(name='Infections', lon=cases_lng, lat=cases_lat, visible=False, hovertemplate=df_cases['text'], text='Text', mode='markers',
+            fig.add_trace(go.Scattergeo(name='Infections', lon=confirmed_lng, lat=confirmed_lat, visible=False, hovertemplate=confirmed_df['text'], text='Text', mode='markers',
                                         marker=dict(size=10, opacity=0.6, color='Blue', symbol='circle'),
                                         ))
-            fig.add_trace(go.Scattergeo(name='Deaths', lon=deaths_lng, lat=deaths_lat, visible=False, hovertemplate=df_deaths['text'], text="Text", mode='markers',
+            fig.add_trace(go.Scattergeo(name='Deaths', lon=deaths_lng, lat=deaths_lat, visible=False, hovertemplate=deaths_df['text'], text="Text", mode='markers',
                                         marker=dict(size=10, opacity=0.6, color='Red', symbol='circle')))
 
             steps = []
@@ -62,8 +62,8 @@ class Grapher():
                         steps=steps
                         )]
 
-            fig.data[0].visible = True
-            fig.data[1].visible = True
+        fig.data[0].visible = True
+        fig.data[1].visible = True
 
         fig.update_geos(
             showcountries=True, countrycolor="RebeccaPurple",
@@ -73,7 +73,6 @@ class Grapher():
                           title='Rise of the Novel Coronavirus<br>A Python Data Visualization by Advait Joshi', title_x=0.5,
                           legend_title='Key',
                           height=600)
-        fig.show()
         go_offline.plot(fig, filename='./map_cov.html', validate=True, auto_open=False)
 
 
